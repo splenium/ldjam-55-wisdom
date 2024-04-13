620597c11b00b6e6eebb6a86dfa089a4a63552c7
Nougato <unconfigured@null.spigotmc.org>
2024-04-13 15:52:30 +0200

    feat: add prefabs player and modificator, also make spawning better

--- godot/assets/player/player.gd
+++ godot/assets/player/player.gd
@@ -6,6 +6,10 @@ class_name Player extends CharacterBody3D
 @export var spawn_sheep_path: PackedScene
 @export var follow_point: Node3D
 
+@export var spawn_x_offset: float = 5.0
+@export var spawn_y_offset: float = 5.0
+@export var spawn_sheep_line: int = 30
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	assert(spawn_sheep_path != null, "spawn_sheep_path is null on " + self.name)
@@ -31,5 +35,8 @@ func apply_sheep_modificator(modificator: int) -> void:
 		for i in range(0, sheepDifference):
 			var instance: Sheep = spawn_sheep_path.instantiate()
 			instance.target = follow_point
+			var middle_offset: float = ((spawn_sheep_line * spawn_x_offset) / 2)
+			instance.position.x = instance.position.x + (i % spawn_sheep_line) * spawn_x_offset - middle_offset
+			instance.position.z += 10 + ceil(i / spawn_sheep_line) * spawn_y_offset
 			self.spawnPointSheep.add_child(instance)
 	pass
