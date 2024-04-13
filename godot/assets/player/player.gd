0d9c1fbf2b25af4fb3725e13c4027fac1c6accf6
Nougato <unconfigured@null.spigotmc.org>
2024-04-13 13:26:25 +0200

    feat: add base bonus and spawn sheep

--- godot/assets/player/player.gd
+++ godot/assets/player/player.gd
@@ -1,21 +1,34 @@
-class_name Player extends CharacterBody2D
+class_name Player extends CharacterBody3D
 
-@export var speed: float = 500.0
+@export var speed: float = 15.0
 @export var sheepNumber: int = 1
+@export var spawnPointSheep: Node3D
+@export var spawn_sheep_path: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    pass # Replace with function body.
-
+	assert(spawn_sheep_path != null, "spawn_sheep_path is null on " + self.name)
+	assert(spawnPointSheep != null, "spawnPointSheep is null on " + self.name)
+	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(d: float) -> void:
-    move(d)
-    pass
-    
+	move(d)
+	pass
+	
 func move(d: float) -> void:
-    if Input.is_action_pressed("ui_left"):
-        self.position.x -= speed * d
-    if Input.is_action_pressed("ui_right"):
-        self.position.x += speed * d
-    pass
+	if Input.is_action_pressed("ui_left"):
+		self.position.x -= speed * d
+	if Input.is_action_pressed("ui_right"):
+		self.position.x += speed * d
+	pass
+	
+func apply_sheep_modificator(modificator: int) -> void:
+	var sheepDifference: int = self.sheepNumber + self.sheepNumber * modificator
+	self.sheepNumber += sheepDifference
+	if sheepDifference > 0:
+		for i in range(0, sheepDifference):
+			var instance: Sheep = spawn_sheep_path.instantiate()
+			instance.target = self
+			self.spawnPointSheep.add_child(spawn_sheep_path.instantiate())
+	pass
---
+++ godot/assets/sheep/sheep.gd
@@ -0,0 +1,22 @@
+class_name Sheep extends RigidBody3D
+
+@export var target: Node3D
+@export var speed: float = 5.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+	
+func look_follow(state: PhysicsDirectBodyState3D, current_transform: Transform3D, target_position: Vector3) -> void:
+	var target_dir: Vector3 = (target_position - current_transform.origin).normalized()
+	state.apply_central_force(target_dir * speed)
+
+func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
+	if target:
+		# Calculer la direction vers la cible
+		#var direction = (target.transform.origin - state.transform.origin).normalized()
+		# Appliquer une force vers la cible
+		#var force = direction * force_magnitude
+		#state.add_central_force(force)
+		look_follow(state, global_transform, target.global_transform.origin)
+	pass
