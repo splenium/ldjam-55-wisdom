11d090a2f35c7d1e46dd27ae44dfbf4838877d52
Benoit Herbigniaux <b.herbigniaux@hotmail.fr>
2024-04-14 01:08:00 +0200

    add correct sound in main menu

--- godot/scripts/CloudUniform.gd
+++ godot/scripts/CloudUniform.gd
@@ -5,10 +5,10 @@ var shader_seed: float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    pass # Replace with function body.
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-    material_override.set("shader_parameter/seed", shader_seed);
-    pass
+	material_override.set("shader_parameter/seed", shader_seed);
+	pass
