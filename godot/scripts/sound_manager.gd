1ec44770217b6f9585ddfa50b51d3c1051128268
Marvin Mottet <11558282+mottet@users.noreply.github.com>
2024-04-14 16:08:35 +0200

    please Godot indentation

--- godot/scripts/sound_manager.gd
+++ godot/scripts/sound_manager.gd
@@ -16,41 +16,41 @@ class_name SoundManager extends Node
 @export var TentaclesSound : AudioStream;
 
 @onready var soundsMap : Dictionary = {
-	"Bomb": BombSound,
-	"Rocket": RocketSound,
-	"Holy": HolySound,
-	"Teleport": TeleportSound,
-	"Bai": BaiSound,
-	"Falling": FallingSound,
-	"Die": DieSound,
-	"Braam": BraamSound,
-	"Hit": HitSound,
-	"Riser": RiserSound,
-	"PassPortal": PortalSound,
-	"Tentacles": TentaclesSound	
+    "Bomb": BombSound,
+    "Rocket": RocketSound,
+    "Holy": HolySound,
+    "Teleport": TeleportSound,
+    "Bai": BaiSound,
+    "Falling": FallingSound,
+    "Die": DieSound,
+    "Braam": BraamSound,
+    "Hit": HitSound,
+    "Riser": RiserSound,
+    "PassPortal": PortalSound,
+    "Tentacles": TentaclesSound	
 }
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	GameManager.soundManager = self
-	pass # Replace with function body.
+    GameManager.soundManager = self
+    pass # Replace with function body.
 
 func PlaySound(key: String) -> void:
-	if not soundsMap.has(key):
-		printerr("Key not found " + key)
-		return
-	var srcAudio : AudioStream  = soundsMap.get(key)
-	var soundComp : AudioStreamPlayer = NoPositionSoundPrefab.instantiate()
-	add_child(soundComp)
-	soundComp.stream = srcAudio
-	soundComp.play()
-	var streamLen : float = srcAudio.get_length()
-	await get_tree().create_timer(streamLen + 1.0).timeout
-	soundComp.queue_free()
-	pass
+    if not soundsMap.has(key):
+        printerr("Key not found " + key)
+        return
+    var srcAudio : AudioStream  = soundsMap.get(key)
+    var soundComp : AudioStreamPlayer = NoPositionSoundPrefab.instantiate()
+    add_child(soundComp)
+    soundComp.stream = srcAudio
+    soundComp.play()
+    var streamLen : float = srcAudio.get_length()
+    await get_tree().create_timer(streamLen + 1.0).timeout
+    soundComp.queue_free()
+    pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_key_pressed(KEY_SPACE):
-		# PlaySound("Bai");
-		pass
-	pass
+    if Input.is_key_pressed(KEY_SPACE):
+        # PlaySound("Bai");
+        pass
+    pass
