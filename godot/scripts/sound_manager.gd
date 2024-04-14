81b6463242cb86367046925fb810493daa2af5b8
seb776 <sebastien.maire@epitech.eu>
2024-04-14 14:35:20 +0200

    Made the audio component

--- godot/scripts/sound_manager.gd
+++ godot/scripts/sound_manager.gd
@@ -2,15 +2,45 @@ extends Node
 
 
 @export var NoPositionSoundPrefab : PackedScene;
-@export var FallingSound : AudioStream;
 
+@export var BombSound : AudioStream;
+@export var RocketSound : AudioStream;
+@export var HolySound : AudioStream;
+@export var TeleportSound : AudioStream;
+@export var BaiSound : AudioStream;
+@export var FallingSound : AudioStream;
+@export var DieSound : AudioStream;
+@export var BraamSound : AudioStream;
+@export var HitSound : AudioStream;
+@export var RiserSound : AudioStream;
+@export var PortalSound : AudioStream;
+@export var TentaclesSound : AudioStream;
+
+@onready var soundsMap : Dictionary = {
+	"Bomb": BombSound,
+	"Rocket": RocketSound,
+	"Holy": HolySound,
+	"Teleport": TeleportSound,
+	"Bai": BaiSound,
+	"Falling": FallingSound,
+	"Die": DieSound,
+	"Braam": BraamSound,
+	"Hit": HitSound,
+	"Riser": RiserSound,
+	"PassPortal": PortalSound,
+	"Tentacles": TentaclesSound	
+}
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-func PlaySound() -> void:
-	var srcAudio : AudioStream  = FallingSound
+func PlaySound(key: String) -> void:
+	if not soundsMap.has(key):
+		printerr("Key not found " + key)
+		return
+	var srcAudio : AudioStream  = soundsMap.get(key)
 	var soundComp : AudioStreamPlayer = NoPositionSoundPrefab.instantiate()
+	add_child(soundComp)
 	soundComp.stream = srcAudio
 	soundComp.play()
 	var streamLen : float = srcAudio.get_length()
@@ -20,13 +50,6 @@ func PlaySound() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
-	
-	pass
-	
-	
-func _unhandled_key_input(event: InputEvent) -> void:
-	if event.is_pressed():
-		PlaySound()
-		pass
+	if Input.is_key_pressed(KEY_SPACE):
+		PlaySound("Bai");
 	pass
