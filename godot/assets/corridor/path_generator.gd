5c4b418b7cf77b9249570cae2daedaad1e899fe9
Marvin Mottet <11558282+mottet@users.noreply.github.com>
2024-04-14 19:55:39 +0200

    add system to build level

--- godot/assets/corridor/path_generator.gd
+++ godot/assets/corridor/path_generator.gd
@@ -5,32 +5,35 @@ class_name PathGenerator extends Node3D
 var first_spawn_position := Vector3(0, 0, despawn_z)
 @export var path_width := 10.0
 
-@export var paths: Array[Node3D] = []
+@export var level_tiles: Array[PackedScene] = []
+@export var displayed_paths: Array[Node3D] = []
 
 func _ready() -> void:
 	var last_path: Node3D = null
-	while (last_path == null || last_path.global_position.z > spawn_z):
+	while ((last_path == null || last_path.global_position.z > spawn_z) && level_tiles.size() > 0):
 		spawn_path()
-		last_path = paths[paths.size() - 1]
+		last_path = displayed_paths[displayed_paths.size() - 1]
 
 func _process(delta: float) -> void:
 	shift_paths(delta)
-	var last_path := paths[paths.size() - 1]
+	var last_path := displayed_paths[displayed_paths.size() - 1]
 	if last_path.global_position.z > spawn_z:
 		spawn_path()
-	if paths.size() > 1 and paths[0].global_position.z > despawn_z:
-		paths[0].queue_free()
-		paths.pop_front()
+	if displayed_paths.size() > 1 and displayed_paths[0].global_position.z > despawn_z:
+		displayed_paths[0].queue_free()
+		displayed_paths.pop_front()
 
 func spawn_path() -> void:
-	var new_path := preload("res://assets/corridor/path_tile.tscn").instantiate()
+	if level_tiles.size() == 0:
+		return
+	var new_path: Node3D = level_tiles.pop_front().instantiate()
 	add_child(new_path)
-	if paths.size() == 0:
+	if displayed_paths.size() == 0:
 		new_path.global_position = first_spawn_position
 	else :
-		new_path.global_position = paths[paths.size() - 1].global_position - Vector3(0, 0, path_width)
-	paths.append(new_path)
+		new_path.global_position = displayed_paths[displayed_paths.size() - 1].global_position - Vector3(0, 0, path_width)
+	displayed_paths.append(new_path)
 
 func shift_paths(delta: float) -> void:
-	for path in paths:
+	for path in displayed_paths:
 		path.global_position += Vector3(0, 0, delta * GameManager.world_speed)
--- godot/assets/modificator/additionator.gd
+++ godot/assets/modificator/additionator.gd
@@ -12,11 +12,6 @@ func _ready() -> void:
 		label3D.text = symbol + str(abs(sheep_to_add))
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(d: float) ->  void:
-	self.position.z += GameManager.world_speed * d
-	pass
-
 func _on_body_entered(body: Node3D) -> void:
 	if body is Player:
 		var player: Player = body;
