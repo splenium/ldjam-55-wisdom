333ab2febcbcc639b02f1fa4b9ef2ea19804c54f
Benoit Herbigniaux <b.herbigniaux@hotmail.fr>
2024-04-14 12:06:32 +0200

    New menu button

--- godot/scenes/main_menu_scene.gd
+++ godot/scenes/main_menu_scene.gd
@@ -2,8 +2,34 @@ extends Node2D
 
 @export var game_scene: PackedScene;
 
+@onready var button_v_box: VBoxContainer = %VBoxContainerBoutons;
+
 var next_scene: PackedScene = game_scene;
 
+func _ready() -> void :
+	focus_button()
+	
 func _on_play_pressed() -> void:
 	next_scene = game_scene;
 	get_tree().change_scene_to_packed(next_scene);
+
+func focus_button() -> void :
+	var button: Button = button_v_box.get_child(0);
+	if button is Button :
+		button.grab_focus()
+
+func _on_credits_focus_entered() -> void:
+	for child in get_node("UI/VBoxContainerBoutons/Credits").get_children():
+		child.visible = true
+
+func _on_credits_focus_exited() -> void:
+	for child in get_node("UI/VBoxContainerBoutons/Credits").get_children():
+		child.visible = false
+
+func _on_play_focus_entered() -> void:
+	for child in get_node("UI/VBoxContainerBoutons/Play").get_children():
+		child.visible = true
+
+func _on_play_focus_exited() -> void:
+	for child in get_node("UI/VBoxContainerBoutons/Play").get_children():
+		child.visible = false
