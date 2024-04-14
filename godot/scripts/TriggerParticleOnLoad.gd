bb2e6159886bd6d629919b6be1872c127a53a6c4
seb776 <sebastien.maire@epitech.eu>
2024-04-14 16:27:05 +0200

    Added explosion particle on rocketsheep

---
+++ godot/scripts/TriggerParticleOnLoad.gd
@@ -0,0 +1,8 @@
+extends CPUParticles3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	restart()
+	emitting = true
+	pass # Replace with function body.
