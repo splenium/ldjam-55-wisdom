ec94fff161185ce095fa7bf341a4647a9586af98
Benoit Herbigniaux <b.herbigniaux@hotmail.fr>
2024-04-14 19:32:37 +0200

    wip

--- godot/scenes/ui/audio_menu_player.gd
+++ godot/scenes/ui/audio_menu_player.gd
@@ -1,6 +1,6 @@
 extends AudioStreamPlayer2D
 
-var music_menue: AudioStreamMP3 = preload("res://music/menu/peritune_material_ominous.mp3");
+var music_menu: AudioStreamMP3 = preload("res://music/menu/peritune_material_ominous.mp3");
 
 func _play_music(music: AudioStream, volume: float = 0.0) -> void:
 	if stream == music:
@@ -10,5 +10,11 @@ func _play_music(music: AudioStream, volume: float = 0.0) -> void:
 	volume_db = volume;
 	play()
 	
+func _stop_music(music: AudioStream) -> void:
+	music.stop()
+	
 func play_music_level() -> void:
-	_play_music(music_menue)
+	_play_music(music_menu)
+	
+func stop_menu_level() -> void:
+	_stop_music(music_menu)
