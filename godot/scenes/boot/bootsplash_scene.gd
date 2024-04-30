c66f2dd0de177f6886b8764aa1650a9e8a069948
Nougato <unconfigured@null.spigotmc.org>
2024-04-30 21:31:49 +0200

    fix: reduce stuttering on web version (v1.2.1)

--- godot/scenes/boot/bootsplash_scene.gd
+++ godot/scenes/boot/bootsplash_scene.gd
@@ -2,9 +2,9 @@ class_name BootsplashScene
 extends Control
 
 @export var fade_duration: float = 1.0
-@export var stay_duration: float = 1.5
 @export var node: PackedScene
 @export var next_scene: PackedScene
+@export var shader_cache: PackedScene
 @export var interuptable: bool = true
 
 @onready var control: Control = %NodeContainer
@@ -18,14 +18,20 @@ func _ready() -> void:
 	tween.set_ease(Tween.EASE_IN)
 	tween.tween_property(instance, "modulate:a", 1.0, fade_duration)\
 	.from(0.0)\
-	.finished.connect(_fade_out)
+	.finished.connect(load_shaders)
+	
+func load_shaders() -> void:
+	if shader_cache:
+		var cache := shader_cache.instantiate()
+		#get_tree().root.add_child(cache) # For kipping always
+		add_child(cache)
+	_fade_out()
 
 func _fade_out() -> void:
 	var tween: Tween = create_tween()
 	tween.set_trans(Tween.TRANS_CUBIC)
 	tween.set_ease(Tween.EASE_IN)
 	tween.tween_property(instance, "modulate:a", 0.0, fade_duration)\
-	.set_delay(stay_duration)\
 	.finished.connect(_change_scene)
 
 func _change_scene() -> void:
--- godot/scripts/ShaderCache.gd
+++ godot/scripts/ShaderCache.gd
@@ -1,13 +1,11 @@
 extends Node3D
 
+@export
 var count_down := 2
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if count_down > 0:
 		count_down = count_down - 1
